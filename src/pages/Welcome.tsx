91e164bd3e160865ae98d7f9bc7ba90691e34d93
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-19 15:02:03 -0300

    Trabalhando com componentes

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView, Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 import wateringImg from '../assets/watering.png'
+import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export  function Welcome(){
@@ -14,9 +15,9 @@ export  function Welcome(){
 
             <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
 
-            <TouchableOpacity style={styles.button} activeOpacity={0.8}>
-                <Text style={styles.buttonText}>></Text>
-            </TouchableOpacity>
+            <Button title=">" />
+            
+
         </SafeAreaView> 
     );
   }
@@ -47,19 +48,5 @@ export  function Welcome(){
       height: 284,
     },
 
-    button:{
-        width:56,
-        height:56,
-        backgroundColor:colors.green,
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:16,
-        marginBottom:10
-    },
-
-    buttonText:{
-        color: colors.white,
-        fontSize:24
-    }
   });
   
