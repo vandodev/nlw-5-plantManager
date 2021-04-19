d70ee932e5dd5c254b46bba9d1dbbec8930135e1
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-19 14:36:41 -0300

    Criando tela de Welcome

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function app (){
   return(
-    // <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-    <View style={style.container}>
-       <Text>nlw 5</Text>
-    </View>
-   
+    <Welcome />
   );
 }
-
-const style = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-})
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {SafeAreaView, Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+import wateringImg from '../assets/watering.png'
+import colors from '../styles/colors';
+
+export  function Welcome(){
+    return(
+        <SafeAreaView style={styles.container}>
+
+            <Text style={styles.title}>Gerencie {'\n'} suas plantas  {'\n'}  de forma fácíl</Text>
+            
+            <Image source={wateringImg} style={styles.image}/>
+
+            <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
+
+            <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+                <Text style={styles.buttonText}>></Text>
+            </TouchableOpacity>
+        </SafeAreaView> 
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  
+    title: {
+      fontSize: 32,
+      fontWeight: "bold",
+      textAlign: "center",
+      color: colors.heading,
+      marginTop: 38,
+    },
+  
+    subtitle: {
+      textAlign: "center",
+      fontSize: 18,
+      paddingHorizontal: 20,
+      color: colors.heading,
+    },
+    image: {
+      width: 292,
+      height: 284,
+    },
+
+    button:{
+        width:56,
+        height:56,
+        backgroundColor:colors.green,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:16,
+        marginBottom:10
+    },
+
+    buttonText:{
+        color: colors.white,
+        fontSize:24
+    }
+  });
+  
