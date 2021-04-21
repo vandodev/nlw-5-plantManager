c126e9a37de33594faa68abc4770f4bf35353721
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-21 08:41:02 -0300

    Criando Header do app

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -5,10 +5,27 @@ import { View, Text, StyleSheet} from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+import {Header} from '../components/Header'
+
 export function PlantSelect() { 
     return(
         <View style={styles.container}>
-            <Text>Selecionar uma planta</Text>
+
+            <View style={styles.header}>
+
+                <Header />
+
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+                </Text>
+
+            </View>
+            
+
         </View>
     );
 }
@@ -16,9 +33,26 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems:'center',
-      justifyContent:'center',
       backgroundColor: colors.background
+    },
+
+    header: {
+        paddingHorizontal: 30
+    },
+
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
     },
    
   })
