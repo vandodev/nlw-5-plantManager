9cd65d4b43a38730f70c4d526978bb5879bb8a83
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-22 10:55:19 -0300

    Formatando a hora da planta salva

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -46,12 +46,27 @@ export interface PlantProps {
 
   }
 
-  export async function loadPlant(): Promise<StoragePlantProps>{
+  export async function loadPlant(): Promise<PlantProps[]>{
     try{
       const data = await AsyncStorage.getItem('@plantmanager:plants');
       const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
-      return plants;
+      const plantsSorted = Object
+      .keys(plants)
+      .map((plant) => {
+        return{
+          ...plants[plant].data,
+          hour:format(new Date(plants[plant].data.dateTimeNotification), 'HH:mm')
+        }
+      })
+      .sort((a,b) =>
+        Math.floor(
+          new Date(a.dateTimeNotification).getTime() / 1000 -
+          Math.floor(new Date(b.dateTimeNotification).getTime() / 1000)
+        )
+      );
+      return plantsSorted;
+
     }catch(error){
       throw new Error(error)
     }
--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -52,17 +52,15 @@ export function PlantSave() {
 }
 
 async function handleSave() {
-  const data = await loadPlant();
-  console.log(data);
-  // try {
-  //   await savePlant({
-  //     ...plant,
-  //     dateTimeNotification: selectedDateTime,
-  //   });
+  try {
+    await savePlant({
+      ...plant,
+      dateTimeNotification: selectedDateTime,
+    });
 
-  // } catch {
-  //   Alert.alert("Não foi possível salvar. 😢");
-  // }
+  } catch {
+    Alert.alert("Não foi possível salvar. 😢");
+  }
 }
 
   return (
