2bfa920a0f324c12add9bf0f1031fe066277629f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-22 10:11:36 -0300

    Refaforando e centralizando interface

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -19,20 +19,10 @@ import waterdrop from "../assets/waterdrop.png";
 import { Button } from "../components/Button";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
+import { PlantProps } from "../libs/storage";
 
   interface Params {
-    plant:{
-      id: string;
-      name:  string;
-      about:  string;
-      water_tips:  string;
-      photo: string;
-      environments: [string];
-      frequency: {
-        times: number;
-        repeat_every: string;
-      }
-    }
+    plant:PlantProps
   }
 
 export function PlantSave() {
