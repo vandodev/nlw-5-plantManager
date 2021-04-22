e1b474218435aa8b3661bf4f45f5b6e3f9b654e9
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-22 11:21:00 -0300

    Refatorando página de confirmação

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -5,30 +5,52 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 import {Button} from '../components/Button';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
+
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug',
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: '🤗',
+  smile: '😄'
+}
+
 
 export function Confirmation() {
 
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const {
+    title,
+    subtitle,
+    buttonTitle,
+    icon,
+    nextScreen
+  } = route.params as Params;
   
   function handleMoveOn() {
-    navigation.navigate('PlantSelect');
+    navigation.navigate(nextScreen);
   }
 
   return (
     <SafeAreaView style={styles.container}>
         <View style={styles.content}>
             <Text style={styles.emoji}>
-                😄
+               {emojis[icon]}
             </Text>
 
             <Text style={styles.title}>
-                Prontinho!
+              {title}
             </Text>
 
             <Text style={styles.subtitle}>
-                Agora vamos começar a cuidar das suas
-                plantinhas com muito cuidado.
+              {subtitle}
             </Text>
 
             <View style={styles.footer}>
--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -32,7 +32,13 @@ export function UserIdentification(){
         return Alert.alert('Me diz como chamar você 😢');
         try{
           await AsyncStorage.setItem('@plantmanager:user', name)
-          navigation.navigate("Confirmation");  
+          navigation.navigate('Confirmation', {
+            title: 'Prontinho',
+            subtitle: `Agora vamos começar a cuidar das suas plantinhas com muito cuidado.`,
+            buttonTitle: 'Começar',
+            icon: 'smile',
+            nextScreen: 'PlantSelect',
+          });  
         }catch{
             Alert.alert('Não foi possivel salvar o seu nome😢');
         }
