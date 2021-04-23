de9bdc875ad3af375988524733deb9a296d63ffc
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-23 02:22:18 -0300

    Função para remover planta da lista

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, View, Image, Text, FlatList, Alert } from "react-native";
 import { Header } from "../components/Header";
 import colors from "../styles/colors";
 
 import waterdrop from "../assets/waterdrop.png";
-import { loadPlant, PlantProps } from "../libs/storage";
+import { loadPlant, PlantProps, StoragePlantProps } from "../libs/storage";
 import { formatDistance } from "date-fns";
 import { pt } from "date-fns/locale";
 import fonts from "../styles/fonts";
@@ -15,6 +16,38 @@ export function MyPlants() {
   const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [nextWatered, setNextWatered] = useState<string>();
+
+  function handleRemove(plant: PlantProps) {
+    Alert.alert('Remover', `Deseja remover a ${plant.name}?`, [
+      {
+        text: 'Não 🙏',
+        style: 'cancel'
+      },
+      {
+        text: 'Sim 🥲',
+        onPress: async () => {
+          try {
+            const data = await AsyncStorage.getItem('@plantmanager:plants');
+            const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+            delete plants[plant.id];
+
+            await AsyncStorage.setItem(
+              '@plantmanager:plants',
+              JSON.stringify(plants)
+            );
+
+            setMyPlants((oldData) =>
+              oldData.filter((item) => item.id != plant.id)
+            )
+
+          } catch (error) {
+            Alert.alert('Não foi possível remover! 🥲');
+          }
+        }
+      }
+    ])
+  }
 
   useEffect(() => {
     async function loadStorageData() {
@@ -56,6 +89,7 @@ export function MyPlants() {
           renderItem={({ item }) => (
             <PlantCardSecondary
               data={item}
+              handleRemove={() => { handleRemove(item) }}
             />
           )}
           showsVerticalScrollIndicator={false}
