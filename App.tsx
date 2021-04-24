c5406e69e37ca783c85be3b4d397c9ab2045b7a7
Evandro oliveira <vandoaparecido@hotmail.com>
2021-04-24 00:10:46 -0300

    Mostrando todas as notificações agendadas no console

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 import Routes from './src/routes/';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function app (){
 
@@ -13,6 +15,15 @@ const [fontsLoaded] = useFonts({
   Jost_400Regular,
   Jost_600SemiBold
 });
+
+useEffect(() => {
+  async function notifications(){
+    const data = await Notifications.getAllScheduledNotificationsAsync();
+    console.log('##### Notificações agendadas #####');
+    console.log(data);
+  }
+  notifications();
+}, [])
 
 if(!fontsLoaded){
   return <AppLoading/>
